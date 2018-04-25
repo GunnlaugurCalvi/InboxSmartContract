c624d00823439bf22d135c7514f1c880e44295d1
Gulli <gunnlaugur15@ru.is>
2018-04-25 21:59:42 +0000

    Basic smart contract project

---
+++ inbox/test/mock.test.js
@@ -0,0 +1,37 @@
+//This file was just for warmup to get to know Mocha
+
+// const assert = require('assert');
+// const ganache = require('ganache-cli');
+// const Web3 = require('web3'); //uppercase Web3 means im working with Web3 constructor function
+// const web3 = new Web3(ganache.provider()); //lowercase web3 means im working with an instance of Web3
+
+// class Car {
+//     park(){
+//         return 'stopped';
+//     }
+    
+//     drive(){
+//         return 'vroom';
+//     }
+// }
+// let car;
+// beforeEach(() => {
+//     car = new Car();
+// });
+
+// describe('Car tests', () => {
+//     it('Park should return a string = stopped', () => {
+//         // const car = new Car();
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it('not stopped', () => {
+//         // const car = new Car();
+//         assert.notEqual(car.park(), 'bitcoin will go 100k');
+//     });
+
+//     it('can drive', () => {
+//         // const car = new Car();
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
